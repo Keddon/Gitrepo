c599f3de24e120d83c399494aa390b7700001e9e
Keddon Campbell <mrkfed101@gmail.com>
2015-04-18 19:47:32 -0400

    main Window Development
    Added my main window and labels to my application window

---
+++ Campbell_Keddon_Project2/build/iphone/build/Campbell_Keddon_Project2.build/Debug-iphonesimulator/Campbell_Keddon_Project2.build/Objects-normal/i386/Mimetypes.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/keddoncampbell/GitHub/VFWc201504/Campbell_Keddon_Project2/build/iphone/Classes/Mimetypes.m \
+  /Users/keddoncampbell/GitHub/VFWc201504/Campbell_Keddon_Project2/build/iphone/Classes/Mimetypes.h
---
+++ Campbell_Keddon_Project2/build/iphone/build/Campbell_Keddon_Project2.build/Debug-iphonesimulator/Campbell_Keddon_Project2.build/Objects-normal/i386/TiUICanvasView.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/keddoncampbell/GitHub/VFWc201504/Campbell_Keddon_Project2/build/iphone/Classes/TiUICanvasView.m
